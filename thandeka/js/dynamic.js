4b9bfb07c41b734f2dc05874d869fc96294d0293
thandeka25 <ttrchehore@gmail.com>
2019-08-18 21:17:13 +0200

    Create dynamic.js

---
+++ thandeka/js/dynamic.js
@@ -0,0 +1,20 @@
+//import array into dynamic.js from my_sneakers.js
+
+
+
+//file to add functions for javascript
+
+
+
+//functions to dynamically populate html table
+
+
+
+//functions to chack for form validation
+
+
+
+//function to append data  to js file and table from form
+
+
+//any extra functions
