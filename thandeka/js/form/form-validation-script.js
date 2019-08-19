b6b5b5e6253495ee5bee5d56f2b40b2da13b7ca2
thandeka25 <ttrchehore@gmail.com>
2019-08-20 00:27:04 +0200

    changed brand validation

--- thandeka/js/form/form-validation-script.js
+++ thandeka/js/form/form-validation-script.js
@@ -11,9 +11,9 @@ var Script = function () {
         // validate signup form on keyup and submit
         $("#register_form").validate({
             rules: {
-                fullname: {
+                brand: {
                     required: true,
-                    minlength: 6
+                    minlength: 1
                 },
                 address: {
                     required: true,
@@ -36,6 +36,10 @@ var Script = function () {
                     required: true,
                     email: true
                 },
+                shoeprice: {
+                    required: true,
+                    number: true
+                },
                 topic: {
                     required: "#newsletter:checked",
                     minlength: 2
@@ -43,9 +47,9 @@ var Script = function () {
                 agree: "required"
             },
             messages: {                
-                fullname: {
-                    required: "Please enter a Full Name.",
-                    minlength: "Your Full Name must consist of at least 6 characters long."
+                brand: {
+                    required: "Please enter a Brand.",
+                    minlength: "Your Brand name must be valid."
                 },
                 address: {
                     required: "Please enter a Address.",
@@ -65,30 +69,12 @@ var Script = function () {
                     equalTo: "Please enter the same password as above."
                 },
                 email: "Please enter a valid email address.",
+                shoeprice: "Please enter a valid price.",
                 agree: "Please accept our terms & condition."
             }
         });
 
-        // propose username by combining first- and lastname
-        $("#username").focus(function() {
-            var firstname = $("#firstname").val();
-            var lastname = $("#lastname").val();
-            if(firstname && lastname && !this.value) {
-                this.value = firstname + "." + lastname;
-            }
-        });
 
-        //code to hide topic selection, disable for demo
-        var newsletter = $("#newsletter");
-        // newsletter topics are optional, hide at first
-        var inital = newsletter.is(":checked");
-        var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-        var topicInputs = topics.find("input").attr("disabled", !inital);
-        // show when newsletter is checked
-        newsletter.click(function() {
-            topics[this.checked ? "removeClass" : "addClass"]("gray");
-            topicInputs.attr("disabled", !this.checked);
-        });
     });
 
 
