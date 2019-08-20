5ec956a34c9005b88760ff7a9595a0a3ba7090fa
thandeka25 <ttrchehore@gmail.com>
2019-08-20 23:12:44 +0200

    make changes

--- thandeka/js/dynamic.js
+++ thandeka/js/dynamic.js
@@ -1,34 +1,33 @@
-const fs = require('fs');
-var data = fs.readFileSync('my_sneakers.json'); //allows for data to be read first before anything else happens
-var sneakercoll = JSON.parse(data); //turn it into js format
-console.log(sneakercoll)
 
 
-function AddInput() {
+function getInput(){
   item = {}  //create empty object
-  //get object's properties from input
-  var brandvalue= document.getElementById('brand').value;
-  var stylevalue= document.getElementById('style').value;
-  var colorvalue= document.getElementById('color').value;
-  var datevalue= document.getElementById('purdate').value;
-  var shoepriceval= document.getElementById('shoeprice').value;
-
+    //get object's properties from input
+    var brandvalue= document.getElementById('brand').value;
+    var stylevalue= document.getElementById('style').value;
+    var colorvalue= document.getElementById('color').value;
+    var datevalue= document.getElementById('purdate').value;
+    var shoepriceval= document.getElementById('shoeprice').value;
   
-  //add properties to object
-  item.Brand = brandvalue;
-  item.Style = stylevalue;
-  item.Color = colorvalue;
-  item.Date = datevalue
-  item.Price = shoepriceval;
+    
+    //add properties to object
+    item.Brand = brandvalue;
+    item.Style = stylevalue;
+    item.Color = colorvalue;
+    item.Date = datevalue
+    item.Price = shoepriceval;
+}
 
 
-  //push object into array
-  sneakercoll.push(item);
-  var sneakercoll = JSON.stringify(sneakercoll) //turn it into json format
-  fs.writeFile(my_sneakers.json, sneakercoll, finished);
-    function finished(err){
-      console.log('all set')
+function addRow(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
     }
-  console.log(sneakercoll)
-  return false  
+  }
 };
+let table = document.querySelector("tbody");
+generateTable(table, formInput);
