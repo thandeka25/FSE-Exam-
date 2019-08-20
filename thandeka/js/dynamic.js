c204c2a2f0af858eec297563fcc6472f0207e80f
thandeka25 <ttrchehore@gmail.com>
2019-08-20 23:54:56 +0200

    edit dynamic.js

--- thandeka/js/dynamic.js
+++ thandeka/js/dynamic.js
@@ -1,33 +1,12 @@
 
-
-function getInput(){
-  item = {}  //create empty object
-    //get object's properties from input
-    var brandvalue= document.getElementById('brand').value;
-    var stylevalue= document.getElementById('style').value;
-    var colorvalue= document.getElementById('color').value;
-    var datevalue= document.getElementById('purdate').value;
-    var shoepriceval= document.getElementById('shoeprice').value;
-  
-    
-    //add properties to object
-    item.Brand = brandvalue;
-    item.Style = stylevalue;
-    item.Color = colorvalue;
-    item.Date = datevalue
-    item.Price = shoepriceval;
-}
-
-
-function addRow(table, data) {
-  for (let element of data) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
-  }
-};
-let table = document.querySelector("tbody");
-generateTable(table, formInput);
+    $(document).ready(function(){
+      $(".add-row").click(function(){
+          var Br = $("#brand").val();
+          var Sty = $("#style").val();
+          var Col = $("#color").val();
+          var Zuva = $("#purdate").val();
+          var Mari = $("#shoeprice").val();
+          var markup = "<tr><td>" + Br +"</td><td>" + Sty + "</td><td>" + Col +  "</td><td>" + Zuva +  "</td><td>" + Mari +  "</td></tr>";
+          $("#dataTable tbody").append(markup);
+      });
+    })
