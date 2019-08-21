6e50714ad0841ca540c526688c69e87199e54dfc
Thandeka <ttrchehore@gmail.com>
2019-08-21 16:29:38 +0200

    updates - table still working

--- thandeka/js/dynamic.js
+++ thandeka/js/dynamic.js
@@ -2,6 +2,4 @@
 const fs = require('fs');
 var data = fs.readFileSync('my_sneakers.json'); //allows for data to be read first before anything else happens
 var sneakercoll = JSON.parse(data); //turn it into js format
-console.log(sneakercoll);
- 
-};
+console.log(sneakercoll); 
