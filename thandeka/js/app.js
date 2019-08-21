753d173aa9fddc824c0c4e0235138a1fe0126a80
Thandeka <ttrchehore@gmail.com>
2019-08-21 16:24:45 +0200

    remove excess code

--- thandeka/js/app.js
+++ thandeka/js/app.js
@@ -14,5 +14,32 @@ var sneakercoll = JSON.parse(data); //turn it into js format
 console.log(sneakercoll);
 
 
-module.exports = sneakercoll;
+
+function AddInput() {
+  item = {}  //create empty object
+  //get object's properties from input
+  var brandvalue= document.getElementById('brand').value;
+  var stylevalue= document.getElementById('style').value;
+  var colorvalue= document.getElementById('color').value;
+  var datevalue= document.getElementById('purdate').value;
+  var shoepriceval= document.getElementById('shoeprice').value;
+
+  
+  //add properties to object
+  item.Brand = brandvalue;
+  item.Style = stylevalue;
+  item.Color = colorvalue;
+  item.Date = datevalue
+  item.Price = shoepriceval;
+
+
+  //push object into array
+  sneakercoll.push(item);
+  var sneakercoll = JSON.stringify(sneakercoll) //turn it into json format
+  fs.writeFile(my_sneakers.json, sneakercoll, finished);
+    function finished(err){
+      console.log('all set')
+    }
+  console.log(sneakercoll)
+  return false 
   
