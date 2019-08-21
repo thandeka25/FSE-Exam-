47d0ae90a0673a6f9cf664e59cd65256ef2bf884
thandeka25 <ttrchehore@gmail.com>
2019-08-21 17:49:16 +0200

    updates

--- thandeka/js/app.js
+++ thandeka/js/app.js
@@ -13,33 +13,15 @@ var data = fs.readFileSync('my_sneakers.json'); //allows for data to be read fir
 var sneakercoll = JSON.parse(data); //turn it into js format
 console.log(sneakercoll);
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.post('/example', (req, res) => {
+  res.send(`Full name is:${req.body.brand} ${req.body.style} ${req.body.color} ${req.body.purdate} ${req.body.shoeprice}.`);
+});
 
 
-function AddInput() {
-  item = {}  //create empty object
-  //get object's properties from input
-  var brandvalue= document.getElementById('brand').value;
-  var stylevalue= document.getElementById('style').value;
-  var colorvalue= document.getElementById('color').value;
-  var datevalue= document.getElementById('purdate').value;
-  var shoepriceval= document.getElementById('shoeprice').value;
 
-  
-  //add properties to object
-  item.Brand = brandvalue;
-  item.Style = stylevalue;
-  item.Color = colorvalue;
-  item.Date = datevalue
-  item.Price = shoepriceval;
-
-
-  //push object into array
-  sneakercoll.push(item);
-  var sneakercoll = JSON.stringify(sneakercoll) //turn it into json format
-  fs.writeFile(my_sneakers.json, sneakercoll, finished);
-    function finished(err){
-      console.log('all set')
-    }
-  console.log(sneakercoll)
-  return false;
-} 
